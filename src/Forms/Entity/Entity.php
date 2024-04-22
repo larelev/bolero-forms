a11601590cf7e410031f5affa138b0098d013eae
David Blanchard <dpjb@hotmail.fr>
2024-04-22 09:25:43 +0200

    Remove override from Entity.php.

--- src/Forms/Entity/Entity.php
+++ src/Forms/Entity/Entity.php
@@ -5,14 +5,12 @@ namespace Bolero\Forms\Entity;
 use Bolero\Forms\Core\StructureInterface;
 use Bolero\Forms\ElementInterface;
 use Bolero\Forms\ElementTrait;
-use Override;
 
 class Entity implements ElementInterface
 {
 
     use ElementTrait;
 
-    #[Override]
     public static function create(StructureInterface $struct): ElementInterface
     {
         return new self($struct);
